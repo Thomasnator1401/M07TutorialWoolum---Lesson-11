2cab89ef573622c363bca1d935fbf4434be0c758
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2023-12-05 22:10:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-let fruits = ['apple', 'banana', 'orange'];
-
-// Using a for loop
-for (let i = 0; i < fruits.length; i++) {
-  console.log(fruits[i]);
-}
-
-// Using a for-of loop
-for (let fruit of fruits) {
-  console.log(fruit);
-}
-
-// Using a forEach method
-fruits.forEach(function(fruit) {
-  console.log(fruit);
+$(document).ready(function() {
+    $('#contactForm').submit(function(e) {
+        e.preventDefault();
+        alert('Thank you for reaching out! We will get back to you soon.');
+    });
 });

dc962c98f51a9e8767c60da84689e1669fdd7b91
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2024-03-11 22:54:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const blogRoutes = require('./routes/blogRoutes');
+const expressRephrased = require('express');
 
-// express app
-const app = express();
+const mongooseRephrased = require('mongoose');
 
-// connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+const authRoutesRephrased = require('./routes/authRoutes');
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
+const cookieParserRephrased = require('cookie-parser');
 
-// register view engine
-app.set('view engine', 'ejs');
+const { requireAuthRephrased, checkUserRephrased } = require('./middleware/authMiddleware');
 
-// middleware & static files
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use((req, res, next) => {
-  res.locals.path = req.path;
-  next();
-});
+
+const appRephrased = expressRephrased();
+
+
+// middleware
+
+appRephrased.use(expressRephrased.static('public'));
+
+appRephrased.use(expressRephrased.json());
+
+appRephrased.use(cookieParserRephrased());
+
+
+// view engine
+
+appRephrased.set('view engine', 'ejs');
+
+
+// database connection
+
+const dbURIRephrased = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+
+mongooseRephrased.connect(dbURIRephrased, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+
+.then((result) => appRephrased.listen(3000))
+
+.catch((err) => console.log(err));
+
 
 // routes
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
-});
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
-});
+appRephrased.get('*', checkUserRephrased);
 
-// blog routes
-app.use('/blogs', blogRoutes);
+appRephrased.get('/', (req, res) => res.render('home'));
 
-// 404 page
-app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
-});
+appRephrased.get('/smoothies', requireAuthRephrased, (req, res) => res.render('smoothies'));
+
+appRephrased.use(authRoutesRephrased);

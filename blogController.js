6524db086854a9667dce78cd4d522dc57e9263b2
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2024-03-11 23:01:42 -0400

    Update blogController.js

--- blogController.js
+++ blogController.js
@@ -1,56 +1,55 @@
-const Blog = require('../models/blog');
+Const blog = require('../fashions/weblog');
 
-const blog_index = (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then(result => {
-      res.render('index', { blogs: result, title: 'All blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-}
+Const blog_index = (req, res) => 
+  blog.Find().Sort( createdAt: -1 )
+    .Then(result => 
+      res.Render('index',  blogs: end result, title: 'All blogs' );
+    )
+    .Capture(err => 
+      console.Log(err);
+    );
 
-const blog_details = (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then(result => {
-      res.render('details', { blog: result, title: 'Blog Details' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-}
 
-const blog_create_get = (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-}
+Const blog_details = (req, res) => 
+  const id = req.Params.Id;
+  blog.FindById(identity)
+    .Then(end result => 
+      res.Render('details',  blog: result, title: 'weblog info' );
+    )
+    .Trap(err => 
+      console.Log(err);
+    );
 
-const blog_create_post = (req, res) => {
-  const blog = new Blog(req.body);
-  blog.save()
-    .then(result => {
-      res.redirect('/blogs');
-    })
-    .catch(err => {
-      console.log(err);
-    });
-}
 
-const blog_delete = (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then(result => {
-      res.json({ redirect: '/blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-}
+Const blog_create_get = (req, res) => 
+  res.Render('create',  name: 'Create a brand new weblog' );
 
-module.exports = {
+
+Const blog_create_post = (req, res) => 
+  const blog = new blog(req.Frame);
+  weblog.Save()
+    .Then(result => 
+      res.Redirect('/blogs');
+    )
+    .Capture(err => 
+      console.Log(err);
+    );
+
+
+Const blog_delete = (req, res) => 
+  const identification = req.Params.Identity;
+  blog.FindByIdAndDelete(identity)
+    .Then(end result => 
+      res.Json( redirect: '/blogs' );
+    )
+    .Capture(err => 
+      console.Log(err);
+    );
+
+
+Module.Exports = 
   blog_index, 
   blog_details, 
   blog_create_get, 
   blog_create_post, 
   blog_delete
-}
